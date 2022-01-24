3b045d27858b8f585d2bcf89b22eda15749cd7d9
Hoang Le Chau <hoangchau612@gmail.com>
2022-01-24 23:57:22 +0700

    Update Project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,105 +1,84 @@
-import "./index.css";
+import './index.css';
 import {
-	initialCards,
-	selectors,
-	modalEditUserInfo,
-	modalAddCard,
-	validationSettings,
-} from "../utils/constants.js";
-import Card from "../components/Card.js";
-import FormValidator from "../components/FormValidator.js";
-import Section from "../components/Section.js";
-import PopupWithImage from "../components/PopupWithImage.js";
-import PopupWithForm from "../components/PopupWithForm.js";
-import UserInfo from "../components/UserInfo.js";
+  initialCards,
+  selectors,
+  modalEditUserInfo,
+  modalAddCard,
+  validationSettings,
+} from '../utils/constants';
+import Card from '../components/Card';
+import FormValidator from '../components/FormValidator';
+import Section from '../components/Section';
+import PopupWithImage from '../components/PopupWithImage';
+import PopupWithForm from '../components/PopupWithForm';
+import UserInfo from '../components/UserInfo';
 
 /* -------------------------------------------------------------------------- */
 /*                               Class Instances                              */
 /* -------------------------------------------------------------------------- */
 
 const userInfo = new UserInfo({
-	nameSelector: modalEditUserInfo.nameSelector,
-	aboutSelector: modalEditUserInfo.aboutSelector,
+  nameSelector: modalEditUserInfo.nameSelector,
+  aboutSelector: modalEditUserInfo.aboutSelector,
 });
 
 const cardPreviewModal = new PopupWithImage(selectors.previewModal);
 cardPreviewModal.setEventListeners();
 
-const createCard = (card) => {
-	return new Card(
-		{
-			data: card,
-			handleCardClick: (imageData) => {
-				cardPreviewModal.open(imageData);
-			},
-		},
-		selectors.cardTemplate
-	);
-};
+const createCard = (card) =>
+  new Card(
+    {
+      data: card,
+      handleCardClick: (imageData) => {
+        cardPreviewModal.open(imageData);
+      },
+    },
+    selectors.cardTemplate,
+  );
 
 const cardSection = new Section(
-	{
-		renderer: (item) => {
-			const cardElement = createCard(item);
-			cardSection.addItem(cardElement.getView());
-		},
-	},
-	selectors.gallery
+  {
+    renderer: (item) => {
+      const cardElement = createCard(item);
+      cardSection.addItem(cardElement.getView());
+    },
+  },
+  selectors.gallery,
 );
 cardSection.renderItems(initialCards);
 
-const editFormSubmitHandler = (evt) => {
-	evt.preventDefault();
-
-	const inputValue = modalEditForm.getInputValues();
-	userInfo.setUserInfo({ name: inputValue.name, about: inputValue.about });
-
-	modalEditForm.close();
-};
-
-const addFormSubmitHandler = (evt) => {
-	evt.preventDefault();
-
-	const inputValue = modalAddForm.getInputValues();
-	const cardElement = createCard({
-		name: inputValue.title,
-		link: inputValue.link,
-	});
-	cardSection.addItem(cardElement.getView());
-
-	modalAddForm.close();
-};
-
 const modalEditForm = new PopupWithForm(
-	modalEditUserInfo.selector,
-	editFormSubmitHandler
+  modalEditUserInfo.selector,
+  (evt) => {
+    evt.preventDefault();
+
+    const inputValue = modalEditForm.getInputValues();
+    userInfo.setUserInfo({
+      name: inputValue.name,
+      about: inputValue.about,
+    });
+
+    modalEditForm.close();
+  },
 );
 modalEditForm.setEventListeners();
 
 const modalAddForm = new PopupWithForm(
-	modalAddCard.selector,
-	addFormSubmitHandler
+  modalAddCard.selector,
+  (evt) => {
+    evt.preventDefault();
+
+    const inputValue = modalAddForm.getInputValues();
+    const cardElement = createCard({
+      name: inputValue.title,
+      link: inputValue.link,
+    });
+    cardSection.addItem(cardElement.getView());
+
+    modalAddForm.close();
+  },
 );
 modalAddForm.setEventListeners();
-
-/* -------------------------------------------------------------------------- */
-/*                               Event Listeners                              */
-/* -------------------------------------------------------------------------- */
-
-modalEditUserInfo.button.addEventListener("click", (evt) => {
-	evt.preventDefault();
-	const profileInfo = userInfo.getUserInfo();
-	modalEditUserInfo.nameInput.value = profileInfo.name;
-	modalEditUserInfo.aboutInput.value = profileInfo.about;
-	formValidators["modalEditForm"].resetValidation();
-	modalEditForm.open();
-});
-
-modalAddCard.button.addEventListener("click", (evt) => {
-	evt.preventDefault();
-	formValidators["modalAddForm"].resetValidation();
-	modalAddForm.open();
-});
 
 /* -------------------------------------------------------------------------- */
 /*                               Form Validation                              */
@@ -107,14 +86,35 @@ modalAddCard.button.addEventListener("click", (evt) => {
 const formValidators = {};
 
 const enableValidation = (config) => {
-	const formList = Array.from(document.querySelectorAll(config.formSelector));
-	formList.forEach((formElement) => {
-		const validator = new FormValidator(config, formElement);
-		const formName = formElement.getAttribute("name");
+  const formList = Array.from(
+    document.querySelectorAll(config.formSelector),
+  );
+  formList.forEach((formElement) => {
+    const validator = new FormValidator(config, formElement);
+    const formName = formElement.getAttribute('name');
 
-		formValidators[formName] = validator;
-		validator.enableValidation();
-	});
+    formValidators[formName] = validator;
+    validator.enableValidation();
+  });
 };
 
 enableValidation(validationSettings);
+
+/* -------------------------------------------------------------------------- */
+/*                               Event Listeners                              */
+/* -------------------------------------------------------------------------- */
+
+modalEditUserInfo.button.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  const profileInfo = userInfo.getUserInfo();
+  modalEditUserInfo.nameInput.value = profileInfo.name;
+  modalEditUserInfo.aboutInput.value = profileInfo.about;
+  formValidators.modalEditForm.resetValidation();
+  modalEditForm.open();
+});
+
+modalAddCard.button.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  formValidators.modalAddForm.resetValidation();
+  modalAddForm.open();
+});
